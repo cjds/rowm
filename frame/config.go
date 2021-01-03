8421727966d915e72f3c6ec59d185b9d594430cb
cjds <cjds@live.com>
2021-01-03 10:09:24 -0800

    fix the batteries

--- frame/config.go
+++ frame/config.go
@@ -10,10 +10,9 @@ import (
 )
 
 type StringWithHelp struct {
-	Data	string
-	Help	string
+	Data string
+	Help string
 }
-
 
 type Config struct {
 	Shell                     string
@@ -34,7 +33,7 @@ type Config struct {
 	PopFrame                  StringWithHelp
 	ResetSize                 string
 	Minimize                  string
-	WindowUp		  StringWithHelp
+	WindowUp                  StringWithHelp
 	WindowDown                StringWithHelp
 	WindowLeft                StringWithHelp
 	WindowRight               StringWithHelp
@@ -112,23 +111,23 @@ func DefaultConfig() Config {
 		Shell:                   "/bin/bash",
 		Lock:                    "Mod4-l",
 		TabByFrame:              true,
-		TabForward:              StringWithHelp{Data: "Mod1-tab", Help:"Tab Forward"},
-		TabBackward:             StringWithHelp{Data: "Mod1-Shift-tab", Help:"Tab Backward"},
+		TabForward:              StringWithHelp{Data: "Mod1-tab", Help: "Tab Forward"},
+		TabBackward:             StringWithHelp{Data: "Mod1-Shift-tab", Help: "Tab Backward"},
 		ButtonDrag:              "1",
 		ButtonClick:             "1",
-		SplitVertical:           StringWithHelp{Data: "Mod4-r", Help:"Split Vertically"},
-		SplitHorizontal:         StringWithHelp{Data: "Mod4-e", Help:"Split Horizontally"},
-		RunCmd:                  StringWithHelp{Data: "Mod4-f", Help:"Run Command"},
+		SplitVertical:           StringWithHelp{Data: "Mod4-r", Help: "Split Vertically"},
+		SplitHorizontal:         StringWithHelp{Data: "Mod4-e", Help: "Split Horizontally"},
+		RunCmd:                  StringWithHelp{Data: "Mod4-f", Help: "Run Command"},
 		Shutdown:                "Mod4-BackSpace",
-		CloseFrame:              StringWithHelp{Data: "Mod4-d", Help:"Close Frame"},
-		ToggleExpandFrame:       StringWithHelp{Data: "Mod4-x", Help:"Toggle Expanded Frame"},
+		CloseFrame:              StringWithHelp{Data: "Mod4-d", Help: "Close Frame"},
+		ToggleExpandFrame:       StringWithHelp{Data: "Mod4-x", Help: "Toggle Expanded Frame"},
 		ToggleExternalDecorator: "Mod4-h",
 		ToggleTaskbar:           "Mod4-s",
-		WindowUp:                 StringWithHelp{Data: "Mod4-up", Help:"Window up"},
-		WindowDown:               StringWithHelp{Data: "Mod4-down", Help:"Window Down"},
-		WindowLeft:               StringWithHelp{Data: "Mod4-left", Help:"Window Left"},
-		WindowRight:              StringWithHelp{Data: "Mod4-right", Help:"Window Right"},
-		PopFrame:                 StringWithHelp{Data: "Mod4-q", Help:"Pop Frame"},
+		WindowUp:                StringWithHelp{Data: "Mod4-up", Help: "Window up"},
+		WindowDown:              StringWithHelp{Data: "Mod4-down", Help: "Window Down"},
+		WindowLeft:              StringWithHelp{Data: "Mod4-left", Help: "Window Left"},
+		WindowRight:             StringWithHelp{Data: "Mod4-right", Help: "Window Right"},
+		PopFrame:                StringWithHelp{Data: "Mod4-q", Help: "Pop Frame"},
 		ResetSize:               "Mod4-Shift-up",
 		Minimize:                "Mod4-Shift-down",
 		VolumeUp:                "Mod4-F3",
@@ -136,8 +135,8 @@ func DefaultConfig() Config {
 		VolumeMute:              "Mod4-F1",
 		BrightnessUp:            "Mod4-F12",
 		BrightnessDown:          "Mod4-F11",
-		FocusNext:               StringWithHelp{Data: "Mod4-Tab", Help:"Focus Next"},
-		FocusPrev:               StringWithHelp{Data: "Mod4-asciitilde", Help:"Focus Previous"},
+		FocusNext:               StringWithHelp{Data: "Mod4-Tab", Help: "Focus Next"},
+		FocusPrev:               StringWithHelp{Data: "Mod4-asciitilde", Help: "Focus Previous"},
 		Backlight:               "intel_backlight",
 		ElemSize:                10,
 		CloseCursor:             xcursor.Dot,
@@ -161,14 +160,14 @@ func DefaultConfig() Config {
 		FocusMarkerTime:     time.Millisecond * 350,
 		DoubleClickTime:     time.Millisecond * 500,
 		BuiltinCommands: map[StringWithHelp]string{
-			StringWithHelp{Data: "Mod4-t", Help:"Terminal"}:  "x-terminal-emulator",
-			StringWithHelp{Data: "Mod4-w", Help:"Chrome"}:  "google-chrome",
-			StringWithHelp{Data: "Mod4-p", Help: "Gnome"}:  "XDG_CURRENT_DESKTOP=GNOME gnome-control-center",
-			StringWithHelp{Data: "Mod4-o", Help:" XDG"}:"xdg-open .",
-			StringWithHelp{Data: "Mod4-F5", Help:"Pause"}: "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause",
+			StringWithHelp{Data: "Mod4-t", Help: "Terminal"}:  "x-terminal-emulator",
+			StringWithHelp{Data: "Mod4-w", Help: "Chrome"}:    "google-chrome",
+			StringWithHelp{Data: "Mod4-p", Help: "Gnome"}:     "XDG_CURRENT_DESKTOP=GNOME gnome-control-center",
+			StringWithHelp{Data: "Mod4-o", Help: " XDG"}:      "xdg-open .",
+			StringWithHelp{Data: "Mod4-F5", Help: "Pause"}:    "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause",
 			StringWithHelp{Data: "Mod4-F4", Help: "Previous"}: "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous",
-			StringWithHelp{Data: "Mod4-F6", Help: "Next"}: "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next",
-			StringWithHelp{Data: "Print", Help: "Screenshot"}:"gnome-screenshot -i",
+			StringWithHelp{Data: "Mod4-F6", Help: "Next"}:     "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next",
+			StringWithHelp{Data: "Print", Help: "Screenshot"}: "gnome-screenshot -i",
 		},
 		TaskbarHeight:        20,
 		TaskbarFontSize:      12,
